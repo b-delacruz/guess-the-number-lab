f36d3bd7f2d66686cd4244be57ca3c2492be9ed6
b-delacruz <brianmdelacruz@gmail.com>
2022-05-31 07:39:07 -0400

    create while loop

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+
   getGuess: function() {
     let guess 
-      while(!guess || guess > this.biggestNum || guess < this.smallestNum)
+      while(!guess || guess > this.biggestNum || guess < this.smallestNum) {
+        guess = parseInt(prompt(`Enter a guess between ${smallestNum} and ${biggestNum}`))
+        if ()
+      }
     }
 
-  }prompt(`Enter a guess between ${smallestNum} and ${biggestNum}`)
+  }
   play: function(){
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
